59b77f2c3db6d50007d226872d53e8e387f386fd
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-03-18 15:52:26 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string frase , cebolinhaFala;
+
+Console.WriteLine("Digite uma frase:");
+
+frase = Console.ReadLine() ?? "";
+cebolinhaFala = frase.Replace("r", "l").Replace("R", "L");
+        
+Console.WriteLine("\nCebolinha falaria:");
+Console.WriteLine(cebolinhaFala);
